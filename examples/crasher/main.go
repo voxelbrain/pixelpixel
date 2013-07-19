50cbc19f9084a602e7be577077269f27f674a963
Alexander Surma <surma@surmair.de>
2013-07-19 14:06:58 +0200

    Add dimension changer

--- examples/crasher/main.go
+++ examples/crasher/main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"image/color"
 	"time"
 
+	"github.com/voxelbrain/pixelpixel/imageutils"
 	"github.com/voxelbrain/pixelpixel/protocol"
 )
 
 func main() {
-	time.AfterFunc(4*time.Second, func() {
-		panic("CRASH")
-	})
 	protocol.ServePixel(func(p *protocol.Pixel) {
-		p.Commit()
-		// Block indefinitely
-		select {}
+		img := imageutils.DimensionChanger(p, 4, 1)
+		for i := 0; i < 5; i++ {
+			if i < 3 {
+				img.Set(i, 0, color.RGBA{uint8(100 + i*70), 0, 0, 255})
+			} else if i == 3 {
+				img.Set(i, 0, color.RGBA{0, 255, 0, 255})
+			} else {
+				panic("CRASH")
+			}
+			p.Commit()
+			time.Sleep(1 * time.Second)
+		}
 	})
 }
