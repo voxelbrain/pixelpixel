259038b4f4a98137840ac524ccf111b76fe97051
Alexander Surma <surma@surmair.de>
2013-07-22 16:04:02 +0200

    Write pixelwall element

--- static/js/protocol.js
+++ static/js/protocol.js
@@ -1,29 +1,27 @@
-var ws = new WebSocket('ws://'+window.location.host+'/ws');
+window.addEventListener('WebComponentsReady', function() {
+	var pw = document.getElementsByTagName('pixel-pixelwall')[0];
+	var ws = new WebSocket('ws://'+window.location.host+'/ws');
 
-var i = 0;
-var type_created = i++;
-var type_change  = i++;
-var type_failure = i++;
+	var i = 0;
+	var type_created = i++;
+	var type_change  = i++;
+	var type_failure = i++;
 
-ws.onclose = function() {
-	console.log('WebSocket closed');
-};
-ws.onmessage = function(ev) {
-	event = JSON.parse(ev.data);
-	switch(event.type) {
-		case type_created:
-		var pixel = document.createElement('pixel-pixel');
-		pixel.id = event.pixel;
-		pixel.key = event.pixel;
-		document.body.appendChild(pixel);
-		break;
-		case type_change:
-		var pixel = document.getElementById(event.pixel);
-		if(pixel)
-			pixel.update();
-		break;
-		case type_failure:
-			console.log('ERROR');
-		break
-	}
-};
+	ws.onclose = function() {
+		console.log('WebSocket closed');
+	};
+	ws.onmessage = function(ev) {
+		event = JSON.parse(ev.data);
+		switch(event.type) {
+			case type_created:
+				pw.pixels += ' '+event.pixel;
+			break;
+			case type_change:
+				pw.update(event.pixel);
+			break;
+			case type_failure:
+				console.log('ERROR');
+			break
+		}
+	};
+});
