e8aef4b9bf80da660c0374c6d476c87a3233a35e
Alexander Surma <surma@surmair.de>
2013-07-24 14:20:51 +0200

    Rewrite crash with actual textual countdown

--- examples/crasher/main.go
+++ examples/crasher/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"image/color"
+	"fmt"
+	"image"
 	"time"
 
 	"github.com/voxelbrain/pixelpixel/imageutils"
@@ -12,15 +13,16 @@ func main() {
 	c := protocol.PixelPusher()
 	img := protocol.NewPixel()
 
-	dImg := imageutils.DimensionChanger(img, 4, 1)
+	dImg := imageutils.DimensionChanger(img, 4, 6)
 	for i := 0; i < 5; i++ {
-		if i < 3 {
-			dImg.Set(i, 0, color.RGBA{uint8(100 + i*70), 0, 0, 255})
-		} else if i == 3 {
-			dImg.Set(i, 0, color.RGBA{0, 255, 0, 255})
-		} else {
+		color := imageutils.Green
+		if i > 3 {
 			panic("CRASH")
+		} else if i == 3 {
+			color = imageutils.Red
 		}
+		imageutils.FillRectangle(dImg, image.Rect(0, 0, 4, 6), imageutils.Black)
+		imageutils.DrawText(dImg, image.Rect(0, 0, 4, 6), color, fmt.Sprintf("%d", 3-i))
 		c <- img
 		time.Sleep(1 * time.Second)
 	}
