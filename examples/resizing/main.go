79ec352d6347582a8b1d63e5fe587ea114c6dcce
Alexander Surma <surma@surmair.de>
2013-07-24 15:31:37 +0200

    Plot times in resizing image

--- examples/resizing/main.go
+++ examples/resizing/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/voxelbrain/pixelpixel/imageutils"
 	"github.com/voxelbrain/pixelpixel/protocol"
 	"image"
 	_ "image/jpeg"
 	"log"
 	"net/http"
+	"time"
 )
 
 const (
@@ -17,8 +19,18 @@ func main() {
 	c := protocol.PixelPusher()
 	img := protocol.NewPixel()
 
+	start := time.Now()
 	bigImg := downloadImage()
+	dlDuration := time.Now().Sub(start)
+
+	start = time.Now()
 	imageutils.Copy(img, bigImg, bigImg.Bounds(), img.Bounds())
+	convDuration := time.Now().Sub(start)
+
+	textImg := imageutils.DimensionChanger(img, 128, 128)
+	r := textImg.Bounds()
+	text := fmt.Sprintf("DL:   %s\nConv: %s", dlDuration, convDuration)
+	imageutils.DrawText(textImg, image.Rect(r.Min.X, r.Max.Y-12, r.Max.X, r.Max.Y), imageutils.Red, text)
 	c <- img
 	select {}
 }
