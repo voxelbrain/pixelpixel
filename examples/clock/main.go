ada042c930e7595a5c94d3a193f23c0c65323cdc
Alexander Surma <surma@surmair.de>
2013-07-25 10:08:20 +0200

    Add reload button to code inspector

--- examples/clock/main.go
+++ examples/clock/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/voxelbrain/pixelpixel/pixelutils"
 	"image"
-	"log"
 	"time"
 )
 
@@ -21,7 +20,6 @@ func main() {
 		} else {
 			colon = ":"
 		}
-		log.Printf("time=\"%s\", colon=\"%s\"", time.Now(), colon)
 		timeStr := fmt.Sprintf("%02d%s%02d", time.Now().Hour(), colon, time.Now().Minute())
 		pixelutils.DrawText(img, image.Rect(0, 6, 5*4, 12), pixelutils.Green, timeStr)
 		c <- pixel
