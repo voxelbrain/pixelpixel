b2580dc6b5a221c540d86ffead8f9189b14ced69
Alexander Surma <surma@surmair.de>
2013-07-29 15:41:16 +0200

    Add SubImager

--- pixelutils/copy.go
+++ pixelutils/copy.go
@@ -13,3 +13,12 @@ func Copy(dst draw.Image, src image.Image, sr, dr image.Rectangle) {
 	resizeImg, _ := resample.Resize(image.Point{dr.Dx(), dr.Dy()}, subImg)
 	draw.Draw(dst, dr, resizeImg, image.Point{0, 0}, draw.Over)
 }
+
+type SubImager interface {
+	draw.Image
+	SubImage(r image.Rectangle) image.Image
+}
+
+func SubImage(img SubImager, r image.Rectangle) draw.Image {
+	return img.SubImage(r).(draw.Image)
+}
