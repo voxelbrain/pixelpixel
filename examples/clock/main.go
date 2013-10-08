837fe23ceac180bd7c61482ed2c570e89b821cda
Alexander Surma <surma@surmair.de>
2013-10-08 12:38:28 +0200

    Use generator for clock example

--- examples/clock/main.go
+++ examples/clock/main.go
@@ -6,23 +6,28 @@ import (
 	"time"
 )
 
+func colonGenerator() <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			c <- ":"
+			c <- " "
+		}
+	}()
+	return c
+}
+
 func main() {
 	wall, _ := pixelutils.PixelPusher()
 	pixel := pixelutils.NewPixel()
 	bigPixel := pixelutils.DimensionChanger(pixel, 5*4, 18)
 	textPixel := pixelutils.NewImageWriter(bigPixel, pixelutils.Green)
 
-	colon := ":"
+	colons := colonGenerator()
 	for {
 		pixelutils.Empty(bigPixel)
-		if colon == ":" {
-			colon = " "
-		} else {
-			colon = ":"
-		}
-
 		textPixel.Cls()
-		fmt.Fprintf(textPixel, "%02d%s%02d", time.Now().Hour(), colon, time.Now().Minute())
+		fmt.Fprintf(textPixel, "%02d%s%02d", time.Now().Hour(), <-colons, time.Now().Minute())
 		wall <- pixel
 		time.Sleep(500 * time.Millisecond)
 	}
